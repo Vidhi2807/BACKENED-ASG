c5dd36f16adffb969b7b33394c1d172a723c8011
Vidhi2807 <vidhimandaliya81@gmail.com>
2026-04-18 08:19:24 +0530

    Update by ID

--- Assignment_1/src/controllers/note.controller.js
+++ Assignment_1/src/controllers/note.controller.js
@@ -70,5 +70,26 @@ const getNotesById = async (req,res)=>{
   }
 }
 
+////update note by id
+const UpdateById = async (req,res) =>{
+  try{
+      const noteId = req.params.id;
+      const {title,content,category} = req.body;
+      const updatedNote = await Note.findByIdAndUpdate(noteId,{title,content,category},{new:true,overwrite : true});
+      if(!updatedNote){
+        return res.status(404).json({msg: "Note Not found"});
+      }
+      res.status(200).json({
+        success: true,
+        msg:"Note updated successfully.",
+        note:updatedNote,
+      })
+  }
+  catch(err){
+      res.status(500).json({msg : "Server error",error:err.message});
+  }
+}
 
-module.exports = { createNote , multipleNotes ,getAllNotes, getNotesById}; 
+
+
+module.exports = { createNote , multipleNotes ,getAllNotes, getNotesById , UpdateById}; 
--- Assignment_1/src/routes/note.routes.js
+++ Assignment_1/src/routes/note.routes.js
@@ -7,6 +7,7 @@ const {
   multipleNotes,
   getAllNotes ,
   getNotesById,
+  UpdateById
   
  
 } = require('../controllers/note.controller.js');
@@ -15,6 +16,8 @@ router.post('/notes', createNote);
 router.post('/multiple', multipleNotes);
 router.get('/notes', getAllNotes);
 router.get('/notes/:id', getNotesById);
+router.put('/:id',UpdateById);
+
 
 
 module.exports = router;
